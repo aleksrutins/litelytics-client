c5416412ed186d835cb60ba92dbc5e5a94f73b99
Aleks Rutins <aleks@rutins.com>
2021-05-14 22:11:03 +0000

    Add stuff

---
+++ index.js
@@ -0,0 +1,16 @@
+export default class Analytics {
+    constructor(instanceURL) {
+        this.instanceURL = instanceURL;
+    }
+    async track() {
+        await fetch(`${this.instanceURL}/api/site/${location.hostname}/track`, {
+        method: "POST",
+        body: JSON.stringify({
+            path: location.pathname
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+        });
+    }
+}

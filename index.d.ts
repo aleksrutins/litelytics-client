efce1c0ecc4113ec91dcd0c27db449dd07605258
Aleks Rutins <alex@rutins.com>
2021-05-15 16:04:51 -0400

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+declare module "litelytics-client" {
+    export default class Analytics {
+        constructor(instanceURL: string);
+        async track(): void;
+    }
+    export namespace auth {
+        type LogInResult = {
+            userId: number,
+            token: string
+        };
+        type SiteInfo = {
+            id: number,
+            domain: string
+        };
+        type Visit = {
+            id: number,
+            site: number,
+            useragent: string,
+            path: string,
+            referer: string,
+            timestamp: string,
+            ip: string
+        };
+        declare function logIn(instanceURL?: string): Promise<LogInResult>;
+        declare function setAuth(token: string, userId: number): void;
+        declare function setInstance(instanceURL: string): void;
+        declare function getSiteInfo(siteId: number): SiteInfo;
+        declare function getSiteData(siteId: number): Visit[];
+    }
+}
